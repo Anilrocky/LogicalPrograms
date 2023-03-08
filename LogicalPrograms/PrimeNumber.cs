b368676ab68bc8b9df424a6dc352d2a2cac2c823
Anilrocky <anilkumarac96@gmail.com>
2023-03-08 16:00:03 +0530

    [Anil] Add:Check number is Prime or not

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+        public void IsPrime(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("{0} is not prime", num);
+                    break;
+                }                    
+                else
+                {
+                    Console.WriteLine("{0} is prime", num);
+                    break;
+                }                    
+            }
+        }
+    }
+}
