d3dba8bd53c0a239313ef6f5107cf2bd1954bcde
Anilrocky <anilkumarac96@gmail.com>
2023-03-08 14:45:31 +0530

    [Anil] Add:Print the Fibonacci series

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,6 +6,24 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an option to execute \n1.Fibonacci Series \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter number");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.PrintSeries(number);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
